ffb490be89dbf32bcb62a9259b58729a75f6d452
MukaDeveloper <muka.bertoni@gmail.com>
2024-09-04 09:25:06 -0300

    feat: factoryEnvelope and factoryEnvelopeArray

--- src/shared/utils/envelope.ts
+++ src/shared/utils/envelope.ts
@@ -3,5 +3,43 @@ export interface IEnvelope<T> {
 }
 
 export interface IEnvelopeArray<T> {
-  items: T;
+  actives?: number;
+  offSet: number;
+  limit: number;
+  itemsTotal: number;
+  total: number;
+  items: T[];
+}
+
+export function factoryEnvelope<T>(data: T | null) {
+  return {
+    item: data || null,
+  };
+}
+
+export function factoryEnvelopeArray<T>(items?: T[]) {
+  if (!items) {
+    items = [];
+  }
+  const data = {} as IEnvelopeArray<T>;
+  data.offSet = 0;
+  data.limit = 0;
+  data.itemsTotal = items.length;
+  data.total = items.length;
+  data.items = items;
+  return data;
+}
+export function factoryEnvelopeArrayPagination<T>(
+  items: T[],
+  offset: number,
+  limit: number,
+  total?: number
+) {
+  const data = {} as IEnvelopeArray<T>;
+  data.offSet = offset;
+  data.limit = limit;
+  data.itemsTotal = items.length;
+  data.total = total || items.length;
+  data.items = items;
+  return data;
 }
