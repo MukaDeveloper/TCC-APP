bbca8c62424497cfde7c5e574cc09840b2d2fbad
Muka <muka.bertoni@gmail.com>
2024-12-08 21:49:42 -0300

    enviroment apontando pra nuvem

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  apiUrl: 'http://localhost:5078',
-  // apiUrl: 'https://api-stocktrack-fuc3b4epc6g3byhm.brazilsouth-01.azurewebsites.net',
+  // apiUrl: 'http://localhost:5078',
+  apiUrl: 'https://api-stocktrack-fuc3b4epc6g3byhm.brazilsouth-01.azurewebsites.net',
   production: false,
   firebaseConfig: {
     apiKey: "AIzaSyBWkNv7131x1DLsPCF8gqI6cCIvu3bCtVM",
